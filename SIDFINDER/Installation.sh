d34be1f8693289d002b70f3497e659ca8fe0a585
Barbaros <69535237+Hellixaz@users.noreply.github.com>
2023-08-31 17:34:23 +0300

    Add Installation.sh file
    Create installation and update requirenments

---
+++ SIDFINDER/Installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printf "----- Installation Starts -----\n"
+
+printf "----- Installation Python -----\n"
+
+apt-get install -y python3-pip
+apt-get install -y python-pip
+apt-get install -y dnspython
