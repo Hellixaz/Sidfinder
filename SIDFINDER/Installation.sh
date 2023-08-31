422e0bec765b2cb1cc5a50366e1c14c146ad7f6d
Barbaros <69535237+Hellixaz@users.noreply.github.com>
2023-08-31 18:47:05 +0300

    Delete SIDFINDER directory

--- SIDFINDER/Installation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "----- Installation Starts -----\n"
-
-printf "----- Installation Python -----\n"
-
-apt-get install -y python3-pip
-apt-get install -y python-pip
-apt-get install -y dnspython
